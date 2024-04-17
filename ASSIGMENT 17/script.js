096750d74a7fe9161e1f637283fe1b91066dec51
Mubashir Ahmed Abdullah <mubashirahmed5468@gmail.com>
2024-04-17 17:59:41 +0500

    done
    start js second assigment

---
+++ ASSIGMENT 17/script.js
@@ -0,0 +1,22 @@
+
+
+// Write a loop to print numbers from 1 to 10
+
+for(var i = 1 ; i<=10; i++){
+    console.log(i)
+}
+
+//Write a loop to print even numbers from 1 to 20
+
+for(var i = 1 ; i <=20; i++){
+    var b = i % 2 == 0;
+    console.log(b)
+}
+
+//Write a loop to print odd numbers from 1 to 15
+
+for( var i =1; i<= 15 ; i++){
+    var c = i % 2 == 1 ;
+    console.log(c)
+}
+
---
+++ JavaScript Assigment 3/script.js
@@ -0,0 +1,23 @@
+
+
+// -----------*String Length Calculation*-------------//
+var myString = "Hello, World!";
+var secondvar = (myString);
+console.log("Length of the string is:", secondvar.length);
+
+
+//--------------*String to Uppercase*-----------------//
+
+var foruppercase = "hello world"
+var secondvartouppercase = (foruppercase)
+console.log(secondvartouppercase.toUpperCase());
+
+
+//--------------*String to Uppercase*-----------------//
+
+var forlowercase = "HELLO WORLD"
+var secondvartolowercase = (forlowercase)
+console.log(secondvartolowercase.toLowerCase());
+
+var sentence1 = "As the sun lazily descended beyond the horizon, casting a warm glow over the tranquil countryside, the gentle rustle of leaves whispered secrets to the evening breeze, while the distant murmur of a flowing stream provided a soothing melody to the ears of nature's silent observers, the birds perched atop swaying branches, the industrious ants scurrying along their well-trodden paths, and the majestic trees standing tall as guardians of the land; meanwhile, in the quaint cottage nestled amidst this picturesque scene, a flickering fire danced in the hearth, casting flickering shadows upon the worn wooden floors, as the aroma of freshly baked bread wafted through the  mingling with the scent of wildflowers that adorned the windowsills, creating a symphony of sensations that enveloped the senses in a comforting embrace, inviting one to momentarily forget the cares of the world outside and bask in the simple joys of existence, where time seemed to slow its relentless march and every moment held the promise of serenity and contentment, like a cherished memory etched forever in the tapestry of the heart"
+console.log(sentence1 .indexOf ("the"));
