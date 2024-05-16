4028822bb899cc963f9a4ed99b67ee8e6ae77cfe
Mubashir Ahmed Abdullah <mubashirahmed5468@gmail.com>
2024-05-16 10:26:57 +0500

    done

---
+++ JavaScript Assigment 12/script.js
@@ -0,0 +1,44 @@
+
+
+var imageupdate = document.getElementById('image');
+var btntext = document.getElementById('switch');
+
+btntext.addEventListener("click", function(){
+    // alert("kam kr raha hay")
+//     var click = "TURN OFF"
+    // btntext.innerText = click ;
+    // btntext.innerText = btntext.innerText ;
+    // btntext.style.backgroundColor = "red";
+
+
+    // imageupdate.src = 'assest/light-bulbs-set-27284743-removebg-preview.png';
+
+//     if(btntext.innerText = "TURN ON"){
+//             btntext.innerText = click ;
+//     btntext.innerText = btntext.innerText ;
+//     btntext.style.backgroundColor = "red";
+
+
+//     imageupdate.src = 'assest/light-bulbs-set-27284743-removebg-preview.png';
+//     }
+//     else{
+//             btntext.innerText = "TURN ON" ;
+//     btntext.innerText = btntext.innerText ;
+//     btntext.style.backgroundColor = "green";
+
+
+//     imageupdate.src = 'assest/light-bulbs-set-27284743-removebg-preview.png';
+//     }
+
+if(btntext.innerText = "TURN ON"){
+        btntext.innerText = "TURN OFF";
+        btntext.style.backgroundColor = "red"
+        imageupdate.src = 'assest/light-bulbs-set-27284743-removebg-preview.png'
+}
+
+if(btntext.innerText = "TURN OFF"){
+        btntext.innerText = "TURN OFF"
+        // btntext.style.backgroundColor = "green"
+        imageupdate.src = 'assest/light-bulbs-set-27284743-removebg-preview.png'
+}
+})
