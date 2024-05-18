8df3e95156b7f799cf46e8cd076cc7ad3055c8ae
Mubashir Ahmed Abdullah <mubashirahmed5468@gmail.com>
2024-05-19 01:13:51 +0500

    done

---
+++ JavaScript Assigment 13/script.js
@@ -0,0 +1,39 @@
+
+
+
+var user_input = document.getElementById('user-text');
+
+function textbold(){
+   user_input.style.fontWeight = 'bolder'
+    
+}
+
+
+function textitalic(){
+    user_input.style.fontStyle = 'italic';
+}
+
+function textunderline(){
+    user_input.style.textDecoration = 'underline';
+}
+
+
+function textsize(){
+    user_input.style.fontSize = '50pt';
+}
+
+
+
+function textfontfamily(){
+    user_input.style.fontFamily = 'Times New Roman';
+}
+
+
+
+function textitalic(){
+    user_input.style.fontStyle = 'italic';
+}
+
+function reset(){
+    document.getElementById('user_input').reset();
+}
