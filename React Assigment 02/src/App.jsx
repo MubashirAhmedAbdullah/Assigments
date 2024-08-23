7cf29df38a017f62b731f1ee593e16c64c391f59
Mubashir Ahmed Abdullah <mubashirahmed5468@gmail.com>
2024-08-23 07:46:45 +0500

    React Assigment 02 image gallery started

---
+++ React Assigment 02/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-4xl mt-8 font-bold underline italic text-red-900'>Image Gallery</h1>
+    </>
+  )
+}
+
+export default App
