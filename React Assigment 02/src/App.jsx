096ef75b7dc526283720ac2bd54deb25c97d0d62
Mubashir Ahmed Abdullah <mubashirahmed5468@gmail.com>
2024-08-30 10:51:49 +0500

    React Assigment 02 started

--- React Assigment 02/src/App.jsx
+++ React Assigment 02/src/App.jsx
@@ -1,13 +1,12 @@
-
 import './App.css'
+import { Titletext } from './components/text'
 
 function App() {
-
-  return (
-    <>
-      <h1 className='text-center text-4xl mt-8 font-bold underline italic text-red-900'>Image Gallery</h1>
-    </>
-  )
+return(
+  <>
+  <Titletext />
+  </>
+)
 }
 
 export default App
---
+++ React Assigment 02/src/components/text.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Titletext(props){
+    return(
+        <div><p className="text-center font-bold pt-10 text-4xl underline italic">Hello World</p></div>
+    )
+}
+
+
+export{
+    Titletext
+}
